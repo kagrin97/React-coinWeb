2d79a15dd8ab8d5a6b5a10ff36d0b73641aebb54
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2022-05-31 11:39:55 +0900

    up

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("build"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(4002, function () {
+  console.log("Express server");
+});
+
+// 서버를 실행하는 과정
+
+// client 폴더로  이동
+// npm ci (node_modules가 없기때문에 만들어야함 ci는 lock을 참조함)
+// npm run build
+// client/build -> server/build 이동
+
+// server 폴더로 이동
+// npm ci (동일하게 모듈파일을 안올렸기때문)
+// node app.js
+
+// 자동으로 실행 하는 방법
+
+// 상위폴더의 package.json 파일에 heroku-pre.. 를 실행시킨다 (명령어 집합)
+// 그리고 npm start를 실행해 server에서 일어날일을 실행시킨다
+// 2로 나누는 이유는 60초이내에 헤로쿠에 배포해야하는데
+// start로만 하면 시간이 안될수도 있기때문이다
