51b9f32a3714f27b2ca1c30d6976648236a749e8
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2022-06-06 13:03:02 +0900

    mobile add

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
+import React from "react";
+import { BrowserView, MobileView } from "react-device-detect";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+
 import "./App.css";
-import React from "react";
+
+import apple from "./사과.png";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <MobileView>
+        <div className="apple">
+          <img src={apple}></img>
+          <h2>죄송합니다</h2>
+          <h3>모바일은 아직 구축하지 못했습니다</h3>
+        </div>
+      </MobileView>
+
+      <BrowserView>
+        <Router>
+          <Routes>
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </BrowserView>
+    </>
   );
 }
 
---
+++ client/src/routes/mobile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Mobile() {
+  return <h2>Abc</h2>;
+}
+
+export default Mobile;
